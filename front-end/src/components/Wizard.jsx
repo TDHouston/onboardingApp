eff9696f60e67b0791599141a9857767b14e6f40
TDHouston <trentdhouston@gmail.com>
2024-08-11 18:50:30 -0400

    cleaning

--- front-end/src/components/Wizard.jsx
+++ front-end/src/components/Wizard.jsx
@@ -43,21 +43,11 @@ const Wizard = () => {
   };
 
   const submitForm = async () => {
-    console.log("Submit button clicked");
-    console.log("Form data:", formData);
-
     try {
-      const response = await apiClient.post("/api/users", formData);
-      console.log("Response from server:", response.data);
+      await apiClient.post("/api/users", formData);
       setShowMessage(true);
     } catch (error) {
       console.error("Error saving data:", error);
-      if (error.response) {
-        console.error("Error Response Status:", error.response.status);
-        console.error("Error Response Data:", error.response.data);
-      } else {
-        console.error("No response received:", error.message);
-      }
     }
   };
 
