23f94ef2bcc1abc74c193cc44394ca8da6007c34
TDHouston <trentdhouston@gmail.com>
2024-08-25 21:32:43 -0400

    uncomment client

--- front-end/src/features/apiClient.jsx
+++ front-end/src/features/apiClient.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  // baseURL: process.env.REACT_APP_API_URL,
-  baseURL: "http://localhost:8080/api",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
